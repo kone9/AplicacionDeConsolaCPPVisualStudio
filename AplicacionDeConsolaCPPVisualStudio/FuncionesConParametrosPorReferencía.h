3960e3e653c851a75c7050b46005c20dcf26dc36
Ariel Gimenez <arielgimenez_95@hotmail.com>
2021-05-15 14:08:49 -0300

    diferenciando entre FUNCION y ACCION con puteros y referencias

--- AplicacionDeConsolaCPPVisualStudio/FuncionesConParametrosPorReferencía.h
+++ AplicacionDeConsolaCPPVisualStudio/FuncionesConParametrosPorReferencía.h
@@ -7,10 +7,10 @@ public:
 	FuncionesConParametrosPorReferencía();//metodo contructor
 
 private:
-	//"FUNCION" funcion comun
+	//"FUNCION" funcion comun que devuelve un valor de tipo entero
 	int Funcion( int valor );
 
-	//"ACCION" funcion con puntero como parametro, recibo la direccion de memoria de la variable, se suele utilizar para modificar valor de variable dentro de funciones de una manera media complicada
+	//"ACCION" funcion con puntero como parametro, recibo la direccion de memoria de la variable , se suele utilizar para modificar valor de variable dentro de funciones de una manera media complicada
 	void FuncionPunteros(int* valor);
 
 	//"ACCION" funcion con parametro por referencía de tipo alias, es como si estuviéramos recibiendo la misma variable original.
